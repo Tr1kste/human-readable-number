6cb44609c684d3b8603329ad2780c81d6ee9a216
Tr1kste <trikster.seo@gmail.com>
2022-01-11 00:03:42 +0300

    feat: fix function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 module.exports = function toReadable(number) {
+    let single = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let teen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let ty = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    let lessThanTen = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let tenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    let twelveToNinety = ['twelve', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    function convertToHundreds(number) {
+        if (number < 10) return single[number];
+        if (number >= 10 && number < 20) return teen[number - 10];
+        if (number >= 20 && number < 100) return ty[Math.floor(number / 10) - 2] + " " + single[number % 10];
+    }
 
+    if (number >= 100) {
+        return single[Math.floor(number / 100)] + " hundred " + convertToHundreds(number % 100);
+    }
     if (number === 0) { return 'zero' };
-    if (number < 10 && number !== 0) { return lessThanTen[number] };
-
-    if (number >= 10 && number < 20) { return tenToNineteen[number - 10] };
-    //else { return twelveToNinety[Math.floor(number / 10)] + " " + lessThanTen[number % 10] };
 }
