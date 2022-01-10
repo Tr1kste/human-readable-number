fce9dcc3f04728c654aa942ef16997aa60beeb7c
Tr1kste <trikster.seo@gmail.com>
2022-01-11 00:44:33 +0300

    feat: final function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 module.exports = function toReadable(number) {
     let single = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     let teen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    let ty = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let ty = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    function convertToHundreds(number) {
+    const convertToHundreds = (number) => {
         if (number < 10) return single[number];
         if (number >= 10 && number < 20) return teen[number - 10];
-        if (number >= 20 && number < 100) return ty[Math.floor(number / 10) - 2] + " " + single[number % 10];
+        if (number >= 20 && number < 100) {
+            const red = `${ty[Math.floor(number / 10) - 2]} ${single[number % 10]}`;
+            return red.replace(/\s$/, '');
+        }
+    }
+
+    if (number < 100 && number !== 0) {
+        return convertToHundreds(number);
     }
 
     if (number >= 100) {
-        return single[Math.floor(number / 100)] + " hundred " + convertToHundreds(number % 100);
+        const blue = `${single[Math.floor(number / 100)]} ${'hundred'} ${convertToHundreds(number % 100)}`;
+        return blue.replace(/\s$/, '');
     }
     if (number === 0) { return 'zero' };
 }
