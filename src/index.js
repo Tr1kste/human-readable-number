d793184dc3d17289821d637e1068a84a6bd0ac1f
Tr1kste <trikster.seo@gmail.com>
2022-01-10 21:59:48 +0300

    feat: start function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    if (number === 0) { return 'zero' }
+    if (number === 1) { return 'one' }
 }
